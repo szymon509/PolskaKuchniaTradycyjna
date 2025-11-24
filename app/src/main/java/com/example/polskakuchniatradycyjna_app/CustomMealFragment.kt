0bad0196906751ee49648b38dc2cb3cd52e43aa4
szymon509 <szymon.gutomski@cyfroweszkoly.pl>
2025-11-24 13:30:58 +0100

    gradle

--- app/src/main/java/com/example/polskakuchniatradycyjna_app/CustomMealFragment.kt
+++ app/src/main/java/com/example/polskakuchniatradycyjna_app/CustomMealFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MenuChoiceFragmentt.newInstance] factory method to
+ * Use the [CustomMealFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MenuChoiceFragmentt : Fragment() {
+class CustomMealFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class MenuChoiceFragmentt : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_choice_fragmentt, container, false)
+        return inflater.inflate(R.layout.fragment_custom_meal, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class MenuChoiceFragmentt : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MenuChoiceFragmentt.
+         * @return A new instance of fragment CustomMealFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MenuChoiceFragmentt().apply {
+            CustomMealFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
