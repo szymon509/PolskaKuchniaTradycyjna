fcd6e19eeeee2f12b4067ce19651d0f94f21b237
szymon509 <szymon.gutomski@cyfroweszkoly.pl>
2025-12-01 13:48:04 +0100

    kt

--- app/src/main/java/com/example/polskakuchniatradycyjna_app/StartFragment.kt
+++ app/src/main/java/com/example/polskakuchniatradycyjna_app/StartFragment.kt
@@ -5,11 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.polskakuchniatradycyjna_app.databinding.StartFragmentBinding
 
-class `StartFragment.kt` : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
+    private lateinit var binding: StartFragmentBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = StartFragmentBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
+        setContentView(binding.root)
         setContentView(R.layout.start_fragment)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
