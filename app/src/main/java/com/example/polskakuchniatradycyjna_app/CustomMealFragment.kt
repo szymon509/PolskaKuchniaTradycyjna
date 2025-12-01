8238b02fad74124cb4300e4914859d7420c6259c
szymon509 <szymon.gutomski@cyfroweszkoly.pl>
2025-12-01 14:02:42 +0100

    bindings

--- app/src/main/java/com/example/polskakuchniatradycyjna_app/CustomMealFragment.kt
+++ app/src/main/java/com/example/polskakuchniatradycyjna_app/CustomMealFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.polskakuchniatradycyjna_app.databinding.FragmentCustomMealBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,6 +19,10 @@ private const val ARG_PARAM2 = "param2"
  */
 class CustomMealFragment : Fragment() {
     // TODO: Rename and change types of parameters
+
+    private var _binding: FragmentCustomMealBinding? = null
+
+    private val binding get() = _binding!!
     private var param1: String? = null
     private var param2: String? = null
 
@@ -34,7 +39,13 @@ class CustomMealFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_custom_meal, container, false)
+        _binding = FragmentCustomMealBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
--- app/src/main/java/com/example/polskakuchniatradycyjna_app/MenuChoiceFragment.kt
+++ app/src/main/java/com/example/polskakuchniatradycyjna_app/MenuChoiceFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.polskakuchniatradycyjna_app.databinding.FragmentMenuChoiceBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,15 +19,14 @@ private const val ARG_PARAM2 = "param2"
  */
 class MenuChoiceFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentMenuChoiceBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -34,7 +34,14 @@ class MenuChoiceFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_choice, container, false)
+
+        _binding = FragmentMenuChoiceBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
